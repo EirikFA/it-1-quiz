bbddc7de895f68f864d8851f1b261a118444f115
Eirik Fossestøl Andresen <eiriand03@gmail.com>
2021-03-31 18:18:32 +0200

    Improve quiz form friendliness

--- src/components/Form/Quiz/index.tsx
+++ src/components/Form/Quiz/index.tsx
@@ -12,6 +12,7 @@ const QuizForm: FunctionComponent<FormikProps<Quiz> & { loading: boolean }> = ({
   values: { questions },
   errors,
   setFieldValue,
+  isValid,
   loading
 }) => {
   const [questionI, setQuestionI] = useState(0);
@@ -60,140 +61,161 @@ const QuizForm: FunctionComponent<FormikProps<Quiz> & { loading: boolean }> = ({
     <FormikForm>
       <Field name="name" label="Quiz name" placeholder="My quiz" component={TextInput} />
 
-      <FieldArray name="questions">
-        {() => (
-          <div className="ml-4">
-            <h4 className="title is-4">Questions</h4>
-            <Columns>
-              <Columns.Column>
-                <h5 className="title is-5">Question #{questionI + 1}</h5>
+      <div className="ml-4">
+        <h4 className="title is-4">Questions</h4>
+        <Columns>
+          <Columns.Column>
+            <Form.Field>
+              <Form.Label>Select question</Form.Label>
+              <Form.Select
+                value={questionI}
+                onChange={e => setQuestionI(parseInt(e.target.value, 10))}
+              >
+                {questions.map((_, index) => (
+                  <option
+                  // eslint-disable-next-line react/no-array-index-key
+                    key={index}
+                    value={index}
+                  >Question #{index + 1}
+                  </option>
+                ))}
+              </Form.Select>
+            </Form.Field>
 
-                <Field
-                  name={`questions[${questionI}].prompt`}
-                  placeholder="Why do Java developers wear glasses?"
-                  component={TextInput}
-                />
-              </Columns.Column>
+            <Field
+              name={`questions[${questionI}].prompt`}
+              placeholder="Why do Java developers wear glasses?"
+              component={TextInput}
+            />
+          </Columns.Column>
 
-              <Columns.Column>
-                <FieldArray name={`questions[${questionI}].options`}>
-                  {({ push, remove }) => {
-                    const questionError = errors.questions
+          <Columns.Column>
+            <FieldArray name={`questions[${questionI}].options`}>
+              {({ push, remove }) => {
+                const questionError = errors.questions
                       && typeof errors.questions !== "string"
-                      ? errors.questions[questionI] : undefined;
+                  ? errors.questions[questionI] : undefined;
 
-                    const optionsError = typeof questionError !== "string"
+                const optionsError = typeof questionError !== "string"
                       && typeof questionError?.options === "string"
-                      ? questionError.options : null;
+                  ? questionError.options : null;
 
-                    return (
-                      <div className="ml-4">
-                        <h5 className="title is-5">
-                          Options
-                          <Icon
-                            className="is-clickable has-text-link ml-3"
-                            onClick={() => push({})}
-                          >
-                            <i className="fas fa-plus" />
-                          </Icon>
-                        </h5>
+                return (
+                  <div className="ml-4">
+                    <h5 className="title is-5">
+                      Options
+                      <Icon
+                        className="is-clickable has-text-link ml-3"
+                        onClick={() => push({
+                          text: "",
+                          is_correct: false
+                        })}
+                      >
+                        <i className="fas fa-plus" />
+                      </Icon>
+                    </h5>
 
-                        <Columns>
-                          {options && options.map(({ is_correct }, oIndex) => (
-                            // eslint-disable-next-line react/no-array-index-key
-                            <Columns.Column key={oIndex} size={6}>
-                              <Form.Field className="is-expanded">
-                                <Form.Field className="has-addons">
-                                  <Form.Control>
-                                    <Button
-                                      color={is_correct ? "success" : "danger"}
-                                      type="button"
-                                      onClick={() => {
-                                        setFieldValue(`questions[${questionI}].options[${oIndex}].is_correct`, !is_correct);
-                                      }}
-                                    >
-                                      <Icon>
-                                        <i className={`fas fa-${is_correct ? "check" : "times"}`} />
-                                      </Icon>
-                                    </Button>
-                                  </Form.Control>
+                    <Columns>
+                      {options && options.map(({ is_correct }, oIndex) => (
+                        // eslint-disable-next-line react/no-array-index-key
+                        <Columns.Column key={oIndex} size={6}>
+                          <Form.Field className="is-expanded">
+                            <Form.Field className="has-addons">
+                              <Form.Control>
+                                <Button
+                                  color={is_correct ? "success" : "danger"}
+                                  type="button"
+                                  onClick={() => {
+                                    setFieldValue(`questions[${questionI}].options[${oIndex}].is_correct`, !is_correct);
+                                  }}
+                                >
+                                  <Icon>
+                                    <i className={`fas fa-${is_correct ? "check" : "times"}`} />
+                                  </Icon>
+                                </Button>
+                              </Form.Control>
 
-                                  <Form.Control>
-                                    <Field
-                                      name={`questions[${questionI}].options[${oIndex}].text`}
-                                      placeholder="Because they can't C#"
-                                      as={Form.Input}
-                                    />
-                                  </Form.Control>
-
-                                  <Form.Control>
-                                    <Button
-                                      color="danger"
-                                      type="button"
-                                      onClick={() => remove(oIndex)}
-                                    >
-                                      <Icon>
-                                        <i className="fas fa-trash-alt" />
-                                      </Icon>
-                                    </Button>
-                                  </Form.Control>
-                                </Form.Field>
-
-                                <ErrorMessage
+                              <Form.Control>
+                                <Field
                                   name={`questions[${questionI}].options[${oIndex}].text`}
-                                  component="p"
-                                  className="help is-danger"
+                                  placeholder="Because they can't C#"
+                                  as={Form.Input}
                                 />
-                              </Form.Field>
-                            </Columns.Column>
-                          ))}
-                        </Columns>
+                              </Form.Control>
 
-                        {optionsError && <p className="help is-danger">{optionsError}</p>}
-                      </div>
-                    );
-                  }}
-                </FieldArray>
-              </Columns.Column>
-            </Columns>
+                              <Form.Control>
+                                <Button
+                                  color="danger"
+                                  type="button"
+                                  onClick={() => remove(oIndex)}
+                                >
+                                  <Icon>
+                                    <i className="fas fa-trash-alt" />
+                                  </Icon>
+                                </Button>
+                              </Form.Control>
+                            </Form.Field>
 
-            <Columns>
-              <Columns.Column>
-                <Button
-                  color="link"
-                  disabled={questionI === 0}
-                  type="button"
-                  onClick={previousQuestion}
-                >
-                  Previous question
-                </Button>
-              </Columns.Column>
+                            <ErrorMessage
+                              name={`questions[${questionI}].options[${oIndex}].text`}
+                              component="p"
+                              className="help is-danger"
+                            />
+                          </Form.Field>
+                        </Columns.Column>
+                      ))}
+                    </Columns>
 
-              <Columns.Column className="has-text-centered">
-                <Button
-                  color="danger"
-                  disabled={questionI === 0}
-                  type="button"
-                  onClick={removeQuestion}
-                >Remove question
-                </Button>
-              </Columns.Column>
+                    {optionsError && <p className="help is-danger">{optionsError}</p>}
+                  </div>
+                );
+              }}
+            </FieldArray>
+          </Columns.Column>
+        </Columns>
 
-              <Columns.Column className="has-text-right">
-                <Button
-                  color="link"
-                  type="button"
-                  onClick={nextQuestion}
-                >
-                  Next question
-                </Button>
-              </Columns.Column>
-            </Columns>
-          </div>
-        )}
-      </FieldArray>
+        <Columns>
+          <Columns.Column>
+            <Button
+              color="link"
+              disabled={questionI === 0}
+              type="button"
+              onClick={previousQuestion}
+            >
+              Previous question
+            </Button>
+          </Columns.Column>
 
-      <Button color="link" loading={loading} type="submit" className="mt-6">Create quiz</Button>
+          <Columns.Column className="has-text-centered">
+            <Button
+              color="danger"
+              disabled={questionI === 0}
+              type="button"
+              onClick={removeQuestion}
+            >Remove question
+            </Button>
+          </Columns.Column>
+
+          <Columns.Column className="has-text-right">
+            <Button
+              color="link"
+              type="button"
+              onClick={nextQuestion}
+            >
+              (Add) next question
+            </Button>
+          </Columns.Column>
+        </Columns>
+      </div>
+
+      <Button
+        color="link"
+        loading={loading}
+        disabled={!isValid}
+        type="submit"
+        className="mt-6"
+      >Create quiz
+      </Button>
     </FormikForm>
   );
 };
